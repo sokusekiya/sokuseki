f5f50426ada9ed6cd7e68caabe18c4d7689a1ff2
june29 <june29.jp@gmail.com>
2018-07-08 23:30:04 +0900

    :octopus: GitHubのUserで認証できる

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ApplicationController
+  def create
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  root to: 'welcome#index'
 end
