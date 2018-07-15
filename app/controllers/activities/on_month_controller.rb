42482591cb4edaa647040e1cc9eb4397626fc2ec
june29 <june29.jp@gmail.com>
2018-07-15 21:16:34 +0900

    :sparkles: サインインが必要な処理にbefore_actionを設定する

--- app/controllers/activities/on_month_controller.rb
+++ app/controllers/activities/on_month_controller.rb
@@ -1,4 +1,6 @@
 class Activities::OnMonthController < ApplicationController
+  before_action :redirect_to_root_unless_signed_in
+
   def index
     @month_string = params[:month_string]
     @activities = current_user.activities.on(@month_string)
