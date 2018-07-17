401f0fb10cc1c933b17b7d85e0bb0a17694b2cde
june29 <june29.jp@gmail.com>
2018-07-17 10:19:31 +0900

    :sparkles: Activityの初回取り込み処理を入れる

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
   def index
     if signed_in?
       @activities = current_user.activities
+
+      if @activities.count.zero?
+        current_user.authentications.each(&:fetch_activities)
+      end
     end
   end
 end
