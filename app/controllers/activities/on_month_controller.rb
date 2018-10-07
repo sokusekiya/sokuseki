23f6ccd4b56d342b38bcf485235e0ffe687ec45a
june29 <june29.jp@gmail.com>
2018-10-07 18:24:49 +0900

    :sparkles: IssueとPull Requestの一覧をMarkdownの箇条書きで出力する

--- app/controllers/activities/on_month_controller.rb
+++ app/controllers/activities/on_month_controller.rb
@@ -4,5 +4,13 @@ class Activities::OnMonthController < ApplicationController
   def index
     @month_string = params[:month_string]
     @activities = current_user.activities.on(@month_string)
+
+    @markdown_content = @activities.issue_and_pr.order(:acted_at).reject { |activity|
+      activity.activity_type == "IssuesEvent" && activity.original_data["action"] != "opened"
+    }.map { |activity|
+      data = activity.original_data
+      target = activity.activity_type == 'IssuesEvent' ? data["issue"] : data["pull_request"]
+      "- [%s](%s)" % [target["title"], target["html_url"]]
+    }.join("\n")
   end
 end
