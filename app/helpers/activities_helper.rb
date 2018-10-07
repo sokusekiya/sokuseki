e593886fe57cd22a4dd29649f8ef72173b0390ef
june29 <june29.jp@gmail.com>
2018-10-07 22:57:15 +0900

    :sparkles: アノテーション付き展開の書き方を間違えていた

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -5,7 +5,7 @@ module ActivitiesHelper
     }.map { |activity|
       data = activity.original_data
       target = (activity.activity_type == "IssuesEvent") ? data["issue"] : data["pull_request"]
-      "- [%{title}](%{url})" % { title: target["title"], url: target["html_url"] }
+      "- [%<title>s}](%<url>s)" % { title: target["title"], url: target["html_url"] }
     }.join("\n")
   end
 end
