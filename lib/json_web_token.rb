b248bdf6a07c507539613bf44fbc9ebb95b5e3a6
yinm <yinm.cminor9@gmail.com>
2019-04-05 13:43:41 +0900

    JWTのコーデックを追加

---
+++ lib/json_web_token.rb
@@ -0,0 +1,18 @@
+class JsonWebToken
+  class << self
+    def encode(payload)
+      JWT.encode(payload, Rails.application.credentials.secret_key_base)
+    end
+
+    def decode(token)
+      HashWithIndiffrentAccess.new(
+        JWT.decode(
+          token,
+          Rails.application.credentials.secret_key_base
+        )[0]
+      )
+    rescue
+      nil
+    end
+  end
+end
