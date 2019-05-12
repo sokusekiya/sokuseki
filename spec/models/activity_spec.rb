b31e12368efa8e956821d477f6c8b26e4e7c0abb
yinm <yinm.cminor9@gmail.com>
2019-05-13 07:53:46 +0900

    何度も使う記述なので短く書きたいのです。
    ref: https://github.com/thoughtbot/factory_bot/blob/21e8bb6bfa4b95149467b128648ba3660c472010/GETTING_STARTED.md#rspec

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe Activity, type: :model do
   describe ".issue_and_pr" do
-    let!(:issue_event) { FactoryBot.create(:activity, :issue_event) }
-    let!(:pull_request_event) { FactoryBot.create(:activity, :pull_request_event) }
-    let!(:commit_comment_event) { FactoryBot.create(:activity, :commit_comment_event) }
+    let!(:issue_event) { create(:activity, :issue_event) }
+    let!(:pull_request_event) { create(:activity, :pull_request_event) }
+    let!(:commit_comment_event) { create(:activity, :commit_comment_event) }
 
     it "IssuesEvent, PullRequestEventだけを取得する" do
       expect(Activity.issue_and_pr).to contain_exactly(issue_event, pull_request_event)
