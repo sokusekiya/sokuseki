34ea60156b3137091c03bca41408137b4a0d5370
tokky <otokita@pepabo.com>
2019-05-15 01:18:37 +0900

    provide year/month info of yearly_activities from controller side

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,9 @@ class WelcomeController < ApplicationController
     if signed_in?
       @activities = current_user.activities
       @yearly_activities = (0..12).each_with_object([]) do |month, activities|
-        activities.push(@activities.on_month_ago(month))
+        on = month.month.ago.year.to_s + "-" + sprintf("%02d", month.month.ago.month.to_s)
+        activity = { on: on, activity: @activities.on_month_ago(month) }
+        activities.push(activity)
       end
 
       current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
