45c880d620b40e62b16a50dd2ede609a5be84589
june29 <june29.jp@gmail.com>
2019-05-16 00:00:27 +0900

    :cop: ガードする

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    if signed_in?
-      @activities = current_user.activities
+    return unless signed_in?
 
-      @annual_activities =
-        @activities.where("acted_at >= ?", 1.year.ago.beginning_of_month).
-          group_by { |a| a.acted_at.strftime("%Y-%m") }.sort_by { |month, _| month }.to_h
+    @activities = current_user.activities
 
-      current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
-    end
+    @annual_activities =
+      @activities.where("acted_at >= ?", 1.year.ago.beginning_of_month).
+        group_by { |a| a.acted_at.strftime("%Y-%m") }.sort_by { |month, _| month }.to_h
+
+    current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
   end
 end
