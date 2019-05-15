bfd7846c7e8879e0ec4d0ba52d31611748ebf8de
june29 <june29.jp@gmail.com>
2019-05-15 23:56:33 +0900

    :sparkles: 処理を整理する

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,10 @@ class WelcomeController < ApplicationController
   def index
     if signed_in?
       @activities = current_user.activities
-      @annual_activities = (0..12).each_with_object([]) do |month_amount, activities|
-        target_month = month_amount.month.ago.year.to_s + "-" + sprintf("%02d", month_amount.month.ago.month.to_s)
-        activity = { on: target_month, activity: @activities.month_ago(month_amount) }
-        activities.push(activity)
-      end
-      @annual_activities.reverse!
+
+      @annual_activities =
+        @activities.where("acted_at >= ?", 1.year.ago.beginning_of_month).
+          group_by { |a| a.acted_at.strftime("%Y-%m") }.sort_by { |month, _|month }.to_h
 
       current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
     end
