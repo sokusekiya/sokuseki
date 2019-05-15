25fa3c6ee9dc8db616a3e08ddc80d528a6e7ad3a
tokky <otokita@pepabo.com>
2019-05-15 22:17:59 +0900

    fix typo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
     if signed_in?
       @activities = current_user.activities
       @annual_activities = (0..12).each_with_object([]) do |month_amount, activities|
-        target_month = month_amount.month.ago.year.to_s + "-" + sprintf("%02d", month_amounth.month.ago.month.to_s)
-        activity = { on: target_month, activity: @activities.on_month_ago(month_amount) }
+        target_month = month_amount.month.ago.year.to_s + "-" + sprintf("%02d", month_amount.month.ago.month.to_s)
+        activity = { on: target_month, activity: @activities.month_ago(month_amount) }
         activities.push(activity)
       end
       @annual_activities.reverse!
