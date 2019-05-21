649c60e7d525eab738ebed4a596b2a22852b401f
osamtimizer <eaten.jb@gmail.com>
2019-05-22 01:50:01 +0900

    annual reportをbarで表示するためにmonthとactivity_typeの複合で集計したい

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
 
     @annual_activities =
       @activities.where("acted_at >= ?", 1.year.ago.beginning_of_month).
-        group("TO_CHAR(acted_at, 'YYYY-MM')").order("TO_CHAR(acted_at, 'YYYY-MM')").count
+        group("TO_CHAR(acted_at, 'YYYY-MM')", :activity_type).order("TO_CHAR(acted_at, 'YYYY-MM')").count
 
     current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
   end
