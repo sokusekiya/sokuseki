b6f75030fab5d1929fbd56210d6572b73e353d50
osamtimizer <eaten.jb@gmail.com>
2019-07-11 23:28:13 +0900

    複数group byは欲しい形で取れないのでinjectで地道に収集する

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,12 @@ class WelcomeController < ApplicationController
 
     @annual_activities =
       @activities.where("acted_at >= ?", 1.year.ago.beginning_of_month).
-        group("TO_CHAR(acted_at, 'YYYY-MM')", :activity_type).order("TO_CHAR(acted_at, 'YYYY-MM')").count
+        group("TO_CHAR(acted_at, 'YYYY-MM')", :activity_type).order("TO_CHAR(acted_at, 'YYYY-MM')").count.inject({}) do |result, (key, value)|
+          acted_at, activity_type = key
+        result[acted_at] ||= {}
+        result[acted_at][activity_type] = value
+        result
+      end
 
     current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
   end
