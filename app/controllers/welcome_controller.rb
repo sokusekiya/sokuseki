1a8267f685529336fc6506d395b9214ef7b5d3ce
osamtimizer <eaten.jb@gmail.com>
2019-07-17 23:35:36 +0900

    injectよりもeach_with_objectを使った方がコードがスリムになる

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,11 +8,10 @@ class WelcomeController < ApplicationController
 
     @annual_activities =
       @activities.where("acted_at >= ?", 1.year.ago.beginning_of_month).
-        group("TO_CHAR(acted_at, 'YYYY-MM')", :activity_type).order("TO_CHAR(acted_at, 'YYYY-MM')").count.inject({}) do |result, (key, value)|
-          acted_at, activity_type = key
-        result[activity_type] ||= {}
-        result[activity_type][acted_at] = value
-        result
+          group("TO_CHAR(acted_at, 'YYYY-MM')", :activity_type).order("TO_CHAR(acted_at, 'YYYY-MM')").count
+          .each_with_object(Hash.new { |h, k| h[k] = {} }) do |(key, value), result|
+        acted_month, activity_type = key
+        result[activity_type][acted_month] = value
       end
 
     current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
