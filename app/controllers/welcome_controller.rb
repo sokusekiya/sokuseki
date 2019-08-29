8bc3802d9131d39df8eb85ebb94e66abb63030ac
june29 <june29.jp@gmail.com>
2019-08-30 00:21:46 +0900

    :muscle: 処理をメソッドに切り出して整理する

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,22 +3,36 @@ class WelcomeController < ApplicationController
     return unless signed_in?
 
     @activities = current_user.activities
-
-    @terms =
-      @activities.map { |a| at = a.acted_at; "%d-%s" % [at.year, %w(1st 2nd)[at.month / 7]] }.uniq.sort.reverse
-    @months =
-      @activities.map { |a| a.acted_at.strftime("%Y-%m") }.uniq.sort.reverse
+    @terms = extract_terms(@activities)
+    @months = extract_months(@activities)
 
     @target_duration = (0..12).map { |n| n.month.ago.strftime("%Y-%m") }.reverse
-
-    @annual_activities =
-      @activities.in_the_last_year.
-        group("TO_CHAR(acted_at, 'YYYY-MM')", :activity_type).order("TO_CHAR(acted_at, 'YYYY-MM')").count.
-        each_with_object(Hash.new { |h, k| h[k] = {} }) do |(key, value), result|
-        acted_month, activity_type = key
-        result[activity_type][acted_month] = value
-      end
+    @annual_activities = extract_annual(@activities)
 
     current_user.authentications.each(&:fetch_activities) if @activities.count.zero?
   end
+
+  private
+
+    def extract_terms(activities)
+      activities.map { |a|
+        at = a.acted_at
+        "%<year>d-%<term>s" % { year: at.year, term: %w[1st 2nd][at.month / 7] }
+      }.uniq.sort.reverse
+    end
+
+    def extract_months(activities)
+      activities.map { |a|
+        a.acted_at.strftime("%Y-%m")
+      }.uniq.sort.reverse
+    end
+
+    def extract_annual(activities)
+      activities.in_the_last_year.
+        group("TO_CHAR(acted_at, 'YYYY-MM')", :activity_type).order("TO_CHAR(acted_at, 'YYYY-MM')").count.
+        each_with_object(Hash.new { |h, k| h[k] = {} }) { |(key, value), result|
+          acted_month, activity_type = key
+          result[activity_type][acted_month] = value
+        }
+    end
 end
