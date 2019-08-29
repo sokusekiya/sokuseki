1ac3d917c7625207ec8aa574ae28063b38dcb686
june29 <june29.jp@gmail.com>
2019-08-30 00:29:46 +0900

    :muscle: Activities::OnMonthという概念をActivities::OnTermに統合する

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   delete "/sessions", to: "sessions#destroy"
 
-  get "/activities/on/:term_string", to: "activities/on_term#index", as: "activities_on_term", constraints: { term_string: /\d{4}-(1st|2nd)/ }
-  get "/activities/on/:month_string", to: "activities/on_month#index", as: "activities_on_month", constraints: { month_string: /\d{4}-\d{2}/ }
+  get "/activities/on/:term_string", to: "activities/on_term#index", as: "activities_on_term"
 
   root to: "welcome#index"
 end
