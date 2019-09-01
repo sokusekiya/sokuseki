23676c430cb8dcea1cfceed443b12c081c191415
june29 <june29.jp@gmail.com>
2019-09-01 21:06:09 +0900

    :muscle: リポジトリごとに分けてMarkdownを出力する

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -9,4 +9,24 @@ module ActivitiesHelper
       "- [%<title>s](%<url>s)" % { title: target["title"], url: target["html_url"] }
     }.uniq.join("\n")
   end
+
+  def markdown_list_group_by_repository(activities)
+    activities.reject { |activity|
+      activity.activity_type == "IssuesEvent" && activity.original_data["action"] != "opened"
+    }.reject { |activity|
+      activity.original_data.dig("repo", "name").nil?
+    }.group_by { |activity|
+      activity.original_data.dig("repo", "name")
+    }.map { |repo_name, repo_activities|
+      list =
+        repo_activities.map { |activity|
+          data = activity.original_data
+          target = (activity.activity_type == "IssuesEvent") ? data["issue"] : data["pull_request"]
+
+          "- [%<title>s](%<url>s)" % { title: target["title"], url: target["html_url"] }
+        }.uniq.join("\n")
+
+      "### #{repo_name}\n\n#{list}\n\n"
+    }.join("\n")
+  end
 end
