f40647ec51e6b2fa8c44a439c3badd3eefc842ff
osamtimizer <eaten.jb@gmail.com>
2019-10-26 23:05:50 +0900

    招待ではない

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -7,7 +7,7 @@ class SharedLinksController < ApplicationController
     token = SecureRandom.hex(32)
     shared_link = SharedLink.new(user: current_user, expired_at: 1.minute.after, on: @term, token: token)
     if shared_link.save
-      flash[:success] = "招待リンクを作成しました"
+      flash[:success] = "共有リンクを作成しました"
     else
       flash[:error] = "エラーが発生しました"
     end
@@ -19,7 +19,7 @@ class SharedLinksController < ApplicationController
     return redirect_to root_path unless current_user.has_available_link_on?(@term)
 
     if current_user.shared_links.available.on(@term).map(&:destroy)
-      flash[:success] = "招待リンクを削除しました"
+      flash[:success] = "共有リンクを削除しました"
     else
       flash[:error] = "エラーが発生しました"
     end
