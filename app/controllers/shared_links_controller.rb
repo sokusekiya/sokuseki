222482a13d069388e1f3ec347e07839d258c4a67
osamtimizer <eaten.jb@gmail.com>
2019-10-26 19:55:32 +0900

    失敗か成功かでフラッシュメッセージは変えた方がよさそう

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -2,8 +2,14 @@ class SharedLinksController < ApplicationController
   before_action :redirect_to_root_unless_signed_in, :shared_link_params
 
   def create
-    SharedLink.create(user: current_user, expired_at: Time.zone.tomorrow, on: params[:on], token: 'hogehoge')
-    redirect_to root_path
+    shared_link = SharedLink.new(user: current_user, expired_at: Time.zone.tomorrow, on: params[:on], token: 'hogehoge')
+    if shared_link.save
+      flash[:success] = "招待リンクを作成しました"
+      redirect_to root_path
+    else
+      flash[:error] = "エラーが発生しました"
+      redirect_to root_path
+    end
   end
 
   def destroy
