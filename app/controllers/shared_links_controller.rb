780c7827d73e942e196af6167506c89f687319f5
osamtimizer <eaten.jb@gmail.com>
2019-10-26 23:05:36 +0900

    model側で判定させる

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -1,11 +1,11 @@
 class SharedLinksController < ApplicationController
-  before_action :redirect_to_root_unless_signed_in, :shared_link_params
+  before_action :redirect_to_root_unless_signed_in, :shared_link_params, :set_term
 
   def create
-    return redirect_to root_path unless current_user.shared_links.available.where(on: shared_link_params[:on]).empty?
+    return redirect_to root_path if current_user.has_available_link_on?(@term)
 
     token = SecureRandom.hex(32)
-    shared_link = SharedLink.new(user: current_user, expired_at: 1.minute.after, on: shared_link_params[:on], token: token)
+    shared_link = SharedLink.new(user: current_user, expired_at: 1.minute.after, on: @term, token: token)
     if shared_link.save
       flash[:success] = "招待リンクを作成しました"
     else
@@ -16,10 +16,10 @@ class SharedLinksController < ApplicationController
   end
 
   def destroy
-    return redirect_to root_path if current_user.shared_links.available.where(on: shared_link_params[:on]).empty?
+    return redirect_to root_path unless current_user.has_available_link_on?(@term)
 
-    if current_user.shared_links.available.on(shared_link_params[:on]).map(&:destroy)
-      flash[:success] = "招待リンクを作成しました"
+    if current_user.shared_links.available.on(@term).map(&:destroy)
+      flash[:success] = "招待リンクを削除しました"
     else
       flash[:error] = "エラーが発生しました"
     end
@@ -31,5 +31,9 @@ class SharedLinksController < ApplicationController
 
     def shared_link_params
       params.permit(:on)
+    end
+
+    def set_term
+      @term ||= shared_link_params[:on]
     end
 end
