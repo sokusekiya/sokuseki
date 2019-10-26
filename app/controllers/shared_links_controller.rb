b39a1a3800a00817bb7ed243ad47ca3cf58cccd8
osamtimizer <eaten.jb@gmail.com>
2019-10-26 19:50:17 +0900

    渡ってきたパラメータを使ってレコードを作成

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -2,6 +2,8 @@ class SharedLinksController < ApplicationController
   before_action :redirect_to_root_unless_signed_in, :shared_link_params
 
   def create
+    SharedLink.create(user: current_user, expired_at: Time.zone.tomorrow, on: params[:on], token: 'hogehoge')
+    redirect_to root_path
   end
 
   def destroy
@@ -9,6 +11,6 @@ class SharedLinksController < ApplicationController
 
   private
   def shared_link_params
-    params.permit(:month, :term)
+    params.permit(:on)
   end
 end
