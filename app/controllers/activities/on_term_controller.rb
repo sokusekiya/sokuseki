5e8ba9d14ecaf602718da4e8f51b4ae07f45d8fe
osamtimizer <eaten.jb@gmail.com>
2019-10-26 20:46:05 +0900

    トークンがあればそのユーザーの情報を、なければログインユーザーの情報を使う

--- app/controllers/activities/on_term_controller.rb
+++ app/controllers/activities/on_term_controller.rb
@@ -1,8 +1,15 @@
 class Activities::OnTermController < ApplicationController
-  before_action :redirect_to_root_unless_signed_in
+  before_action :redirect_to_root_unless_signed_in, :on_term_params
 
   def index
+    user = SharedLink.available.find_by(token: on_term_params[:token])&.user || current_user
     @term_string = params[:term_string]
-    @activities = current_user.activities.on(@term_string)
+    @activities = user.activities.on(@term_string)
   end
+
+  private
+
+    def on_term_params
+      params.permit(:term_string, :token)
+    end
 end
