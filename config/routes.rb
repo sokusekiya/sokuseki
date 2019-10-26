d97a8760bcbc382a1f64e646df25924915a48ce6
osamtimizer <eaten.jb@gmail.com>
2019-10-26 19:09:52 +0900

    resources/:idで扱うのが難しいので単純なルーティングに変える

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   delete "/sessions", to: "sessions#destroy"
-  resources :shared_links, only: [:create, :destroy]
+  post "/shared_link", to: "shared_links#create"
+  delete "/shared_link", to: "shared_links#destroy"
 
   get "/activities/on/:term_string", to: "activities/on_term#index", as: "activities_on_term"
 
