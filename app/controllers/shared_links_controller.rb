35e8f6d16b931d402f763dacb2228640eaa95508
osamtimizer <eaten.jb@gmail.com>
2019-10-26 22:51:15 +0900

    strong parameterを使いましょう

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -2,10 +2,10 @@ class SharedLinksController < ApplicationController
   before_action :redirect_to_root_unless_signed_in, :shared_link_params
 
   def create
-    return redirect_to root_path unless current_user.shared_links.available.where(on: params[:on]).empty?
+    return redirect_to root_path unless current_user.shared_links.available.where(on: shared_link_params[:on]).empty?
 
     token = SecureRandom.hex(32)
-    shared_link = SharedLink.new(user: current_user, expired_at: Time.zone.tomorrow, on: params[:on], token: token)
+    shared_link = SharedLink.new(user: current_user, expired_at: 1.minute.after, on: shared_link_params[:on], token: token)
     if shared_link.save
       flash[:success] = "招待リンクを作成しました"
     else
@@ -16,9 +16,9 @@ class SharedLinksController < ApplicationController
   end
 
   def destroy
-    return redirect_to root_path if current_user.shared_links.available.where(on: params[:on]).empty?
+    return redirect_to root_path if current_user.shared_links.available.where(on: shared_link_params[:on]).empty?
 
-    if current_user.shared_links.available.on(params[:on]).first.destroy
+    if current_user.shared_links.available.on(shared_link_params[:on]).first.destroy
       flash[:success] = "招待リンクを作成しました"
     else
       flash[:error] = "エラーが発生しました"
