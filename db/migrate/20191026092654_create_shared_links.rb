c02906e34588d11feb5b66fa3bba702e4c00a682
osamtimizer <eaten.jb@gmail.com>
2019-10-26 18:31:19 +0900

    全て必須なので

--- db/migrate/20191026092654_create_shared_links.rb
+++ db/migrate/20191026092654_create_shared_links.rb
@@ -2,9 +2,9 @@ class CreateSharedLinks < ActiveRecord::Migration[6.0]
   def change
     create_table :shared_links do |t|
       t.references :user, null: false, foreign_key: true
-      t.datetime :expired_at
-      t.string :on
-      t.string :token
+      t.datetime :expired_at, null: false
+      t.string :on, null: false
+      t.string :token, null: false
 
       t.timestamps
     end
