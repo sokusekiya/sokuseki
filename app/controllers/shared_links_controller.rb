380eecfe1e1c106ea0bfa13df6cb166a30cfef09
osamtimizer <eaten.jb@gmail.com>
2019-10-26 19:38:50 +0900

    必要なbefore actionを定義

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -1,7 +1,14 @@
 class SharedLinksController < ApplicationController
+  before_action :redirect_to_root_unless_signed_in, :shared_link_params
+
   def create
   end
 
   def destroy
+  end
+
+  private
+  def shared_link_params
+    params.permit(:month, :term)
   end
 end
