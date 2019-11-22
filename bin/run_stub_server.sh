7a13c9a4d563309c10690a259cf71ad02b4ba46f
yinm <yinm.cminor9@gmail.com>
2019-11-22 23:41:09 +0900

    OpenAPIドキュメントのバージョンに合わせる必要がある

--- bin/run_stub_server.sh
+++ bin/run_stub_server.sh
@@ -2,4 +2,4 @@
 
 docker run --rm -p 3000:3000 \
   -v ${PWD}/stub_server:/usr/src/myapp -w /usr/src/myapp \
-  java java -jar target/openapi-spring-1.0.0.jar
+  java java -jar target/openapi-spring-0.0.1.jar
