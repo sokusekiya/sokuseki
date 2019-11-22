0fbbca12991b10ee99fe4425056faca7cae0a02a
yinm <yinm.cminor9@gmail.com>
2019-11-22 22:55:47 +0900

    スキーマファイルはbundleしないと、Stub Serverの生成がうまくできない (exampleがあるのに、Not Implementedなエンドポイントになる)
    今日も Joe-noh さんに感謝して生きていこうな

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development do
   gem "listen", ">= 3.0.5", "< 3.3"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+  gem 'yaml_ref_resolver'
   gem "web-console", ">= 3.3.0"
 end
 
--- bin/generate_stub_server.sh
+++ bin/generate_stub_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+docker-compose run --no-deps --rm web bundle exec yaml_ref_resolver -i api_schema/index.yml -o bundled_openapi.yml
+
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.2.0 generate \
-    -i /local/api_schema/index.yml \
+    -i /local/bundled_openapi.yml \
     -g spring \
     -o /local/stub_server
