c279a9cd6a0fc32269691945bc56c56668851923
yinm <yinm.cminor9@gmail.com>
2019-11-22 23:13:34 +0900

    公式のRECOMMENDEDに従って、ファイル名を`openapi`にする (拡張子はRailsのYAMLファイルに合わせて、`.yml`にした)
    https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#document-structure

--- bin/generate_stub_server.sh
+++ bin/generate_stub_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose run --no-deps --rm web bundle exec yaml_ref_resolver -i api_schema/index.yml -o bundled_openapi.yml
+docker-compose run --no-deps --rm web bundle exec yaml_ref_resolver -i api_schema/openapi.yml -o bundled_openapi.yml
 
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.2.0 generate \
     -i /local/bundled_openapi.yml \
