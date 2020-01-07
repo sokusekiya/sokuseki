8eb21e9c3587954151b7ce460833ae037e60abca
osamtimizer <eaten.jb@gmail.com>
2020-01-07 23:37:53 +0900

    最低限のvalidationを確認するテストを書く

--- spec/models/shared_link_spec.rb
+++ spec/models/shared_link_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe SharedLink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  before do
+    @shared_link = SharedLink.new(token: SecureRandom.hex(32), expired_at: 30.minutes.after, on: "2019-12", user: user)
+  end
+
+  subject { @shared_link }
+
+  describe "validation" do
+    it { is_expected.to be_valid }
+  end
 end
