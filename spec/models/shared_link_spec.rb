51ca71cd12699f23ecea44a81eaf8df931956b31
osamtimizer <eaten.jb@gmail.com>
2020-01-08 00:00:58 +0900

    onの存在性をテスト

--- spec/models/shared_link_spec.rb
+++ spec/models/shared_link_spec.rb
@@ -2,13 +2,22 @@ require "rails_helper"
 
 RSpec.describe SharedLink, type: :model do
   let(:user) { create(:user) }
-  before do
-    @shared_link = SharedLink.new(token: SecureRandom.hex(32), expired_at: 30.minutes.after, on: "2019-12", user: user)
-  end
+  let(:token) { SecureRandom.hex(32) }
+  let(:expired_at) { 30.minutes.after }
+  let(:on) { '2019-12' }
 
-  subject { @shared_link }
+  subject do
+    SharedLink.new(token: token, expired_at: expired_at, on: on, user: user)
+  end
 
   describe "validation" do
     it { is_expected.to be_valid }
+
+    context "on is nil" do
+      let(:on) {
+        nil
+      }
+      it { is_expected.to be_invalid }
+    end
   end
 end
