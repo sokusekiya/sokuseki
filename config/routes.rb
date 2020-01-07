4e0e2dcb63fd6d7b6009691598db8e941dab3a53
osamtimizer <eaten.jb@gmail.com>
2020-01-07 11:51:27 +0900

    sokusekiではresource routingを使わない

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   delete "/sessions", to: "sessions#destroy"
   resources :shared_links, only: [:show, :create, :destroy], param: :token
+  get "shared_links/:token", to: "shared_links#show"
+  post "shared_links", to: "shared_links#create"
+  delete "shared_links/:token", to: "shared_links#destroy"
 
   get "/activities/on/:term_string", to: "activities/on_term#index", as: "activities_on_term"
 
