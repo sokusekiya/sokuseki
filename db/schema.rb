23a47dd3850fc847f33315f02fe4c90e27231fa2
osamtimizer <eaten.jb@gmail.com>
2020-01-07 22:57:12 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_092654) do
+ActiveRecord::Schema.define(version: 2020_01_07_134431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_10_26_092654) do
     t.string "token", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["token"], name: "index_shared_links_on_token", unique: true
     t.index ["user_id"], name: "index_shared_links_on_user_id"
   end
 
