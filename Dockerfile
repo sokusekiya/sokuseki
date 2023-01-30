c88a99c2b9d2decc248d3800d1f2d4575b47e419
pyama <pyama@pepabo.com>
2023-01-30 11:14:12 +0900

    use elasticapm

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:3.0.0
+FROM ruby:3.1.3
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 RUN mkdir /myapp
 WORKDIR /myapp
--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_134431) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_01_07_134431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,10 +19,10 @@ ActiveRecord::Schema.define(version: 2020_01_07_134431) do
     t.bigint "authentication_id"
     t.string "activity_id", null: false
     t.string "activity_type", null: false
-    t.datetime "acted_at", null: false
+    t.datetime "acted_at", precision: nil, null: false
     t.jsonb "original_data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["acted_at"], name: "index_activities_on_acted_at"
     t.index ["activity_id", "activity_type"], name: "index_activities_on_activity_id_and_activity_type", unique: true
     t.index ["authentication_id"], name: "index_activities_on_authentication_id"
@@ -36,18 +35,18 @@ ActiveRecord::Schema.define(version: 2020_01_07_134431) do
     t.string "uid", null: false
     t.string "name", null: false
     t.string "access_token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_authentications_on_user_id"
   end
 
   create_table "shared_links", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.datetime "expired_at", null: false
+    t.datetime "expired_at", precision: nil, null: false
     t.string "on", null: false
     t.string "token", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["token"], name: "index_shared_links_on_token", unique: true
     t.index ["user_id"], name: "index_shared_links_on_user_id"
   end
@@ -55,8 +54,8 @@ ActiveRecord::Schema.define(version: 2020_01_07_134431) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "avatar_url", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
